8d58e8a11738c11a16cb3f35025619de41349d40
edabot <ed.lewis@gmail.com>
2016-01-25 22:37:16 +0000

    extract redundancy and prepare for prod

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
    def index
        @articles = Article.all
    end
@@ -8,22 +10,9 @@ class ArticlesController < ApplicationController
    end 
    
    def edit
-       @article = Article.find(params[:id])
-   end
-   
-   def update
-     @article = Article.find(params[:id])
-     if @article.update(article_params)
-         flash[:notice] = 'Article was successfully updated'
-         redirect_to article_path(@article)
-     else
-         render 'edit'
-     end
-
    end
    
    def create
-     # rails consolrender plain: params[:article].inspect
      @article = Article.new(article_params)
      if @article.save
         flash[:notice] = "Article was successfully created"
@@ -33,19 +22,30 @@ class ArticlesController < ApplicationController
      end
    end
    
+   def update
+     if @article.update(article_params)
+         flash[:notice] = 'Article was successfully updated'
+         redirect_to article_path(@article)
+     else
+         render 'edit'
+     end
+   end
+   
    def show
-     @article = Article.find(params[:id]) 
    end
    
    def destroy
-     @article = Article.find(params[:id]) 
      @article.destroy
      flash[:notice] = 'Article was successfully deleted'
      redirect_to articles_path
    end
    
    private
-   def article_params
-       params.require(:article).permit(:title, :description)
-   end
+     def set_article
+        @article = Article.find(params[:id]) 
+     end
+         
+     def article_params
+          params.require(:article).permit(:title, :description)
+     end
 end
