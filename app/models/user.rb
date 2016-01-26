e2c245784237ca4653931d662265a644c99d6260
edabot <ed.lewis@gmail.com>
2016-01-26 22:46:45 +0000

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
-  
+  validates :username, presence: true, 
+                      uniqueness: {case_sensitive: false},
+                      length: { minimum: 3, maximum: 50 }
+  VALID_USER_REGEX = /@/i
+  validates :email, presence:true, length: { maximum: 125 },
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_USER_REGEX }
 end
