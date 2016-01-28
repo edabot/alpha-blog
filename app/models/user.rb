3b471e2735d4ce62aab593659970c4abee76dbdf
edabot <ed.lewis@gmail.com>
2016-01-28 04:55:49 +0000

    add user data to articles page

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:true, length: { maximum: 125 },
                     uniqueness: { case_sensitive: false },
-                    format: { with: VALID_USER_REGEX }
+                    format: { with: VALID_EMAIL_REGEX }
 end
