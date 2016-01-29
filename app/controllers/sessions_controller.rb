460ffe6d1a77f8d3c8095284be519387855f54e4
edabot <ed.lewis@gmail.com>
2016-01-29 04:34:40 +0000

    add login form

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  
+  def new #renders a form
+    
+  end
+  
+  def create #handles form submission, starts session as logged in
+    debugger
+  end
+  
+  def destroy #stopping session, logging out
+    
+  end
+  
+end
