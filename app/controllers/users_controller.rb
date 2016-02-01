7e264ab4f4b9a0d12d39c1c1e36e7096a4ba524a
edabot <ed.lewis@gmail.com>
2016-02-01 05:23:14 +0000

    add sign in upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save  #successfully create user
+    session[:user_id] = @user.id
       flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else  #fail, then show again w messages
       render 'new'
     end
