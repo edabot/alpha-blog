eb08f2b3a7a7955ae857dc04d5abb77fd580c3f6
edabot <ed.lewis@gmail.com>
2016-02-01 04:30:36 +0000

    add a check to session status for navigation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  before_action :set_user, only: [:edit, :update, :show] 
   has_many :articles
   before_save { self.email = email.downcase }
   validates :username, presence: true, 
